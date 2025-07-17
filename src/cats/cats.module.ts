044941dfeac8d41e939d16acccf1dac7608859b9
GBvaillant <vaillantgabriel@gmail.com>
2025-07-17 14:53:30 -0300

    feat: add endpoint for http request

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,10 +3,11 @@ import { CatsService } from './services/cats.service';
 import { CatsController } from './controllers/cats.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cat, CatSchema } from './entities/cat.entity';
+import { S3Service } from 'src/aws/s3.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, S3Service],
 })
 export class CatsModule {}
