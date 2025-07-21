65b8ff882e6195f3fb7241e7b75194325514c8a6
GBvaillant <vaillantgabriel@gmail.com>
2025-07-21 15:14:23 -0300

    feat: modify class for mongoose to work correctly

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     CatsModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/cats'),
+    MongooseModule.forRoot(
+      'mongodb://admin:12345@localhost:27017/cats?authSource=admin',
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
